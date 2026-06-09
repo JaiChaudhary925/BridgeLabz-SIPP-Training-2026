4dece4628420e55c67542afcf05bcb196fefa04c
JAI CHAUDHARY <chaudharyjai6269@gmail.com>
2026-06-09 11:05:45 +0530

    Adding the remaining questions of Level 1

---
+++ core-java-practice/gcr-codebase/programming-elements/HandShakeCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HandshakeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int numberOfStudents = scanner.nextInt();
+
+        int handshakes = (numberOfStudents * (numberOfStudents - 1)) / 2;
+
+        System.out.println("Maximum possible handshakes = " + handshakes);
+
+        scanner.close();
+    }
+}
