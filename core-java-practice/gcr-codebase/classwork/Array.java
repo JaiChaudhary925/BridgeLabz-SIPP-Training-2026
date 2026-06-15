09a15f98b3c890cdd9e05e474e8e930b129fea67
JAI CHAUDHARY <chaudharyjai6269@gmail.com>
2026-06-15 22:58:31 +0530

    Adding the files

---
+++ core-java-practice/gcr-codebase/classwork/Array.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Array{
+	public static void main(String args[]){
+		Object arr1[]={1, 1.1, 111.222, "ABC", "A"};
+		ArrayList<Object> a1=new ArrayList<>();
+		a1.add(arr1[0]);
+		a1.add(arr1[1]);
+		a1.add(arr1[2]);
+		a1.add(arr1[3]);
+		a1.add(arr1[4]);
+	System.out.println(a1);
+	}
+}
